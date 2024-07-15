2467b6f256e7c612c5fae60a7797bd8efd4a2975
238728 <uupeter@126.com>
2024-07-15 13:40:07 +0800

    Add setup_nodenpm.sh && FontawesomePro CSS

---
+++ setup_nodenpm.sh
@@ -0,0 +1,10 @@
+sudo apt install nodejs
+sudo apt install npm
+sudo npm install -g n
+sudo n latest
+echo "Your Current Node Version:"
+node -v
+sudo npm install npm@latest -g
+echo "Your Current NPM Version:"
+npm -v
+echo "Finished installing! ^_^ Script Aborting..."
